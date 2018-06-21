68e39db19580633fd480880cafdfc43082f9e10e
Krish Lalwani <35841954+KrishLalwani@users.noreply.github.com>
2018-06-21 17:51:41 +0530

    lab wise view added

--- viewlab.php
+++ viewlab.php
@@ -26,7 +26,6 @@
     <h1>LABS</h1>
     </div>
     <?php
-
         if ( isset($_SESSION['success']))
         {
             echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
@@ -38,13 +37,12 @@
             unset($_SESSION['error']);
         }
         //echo('<p><a href="logout.php">Logout</a></p>');
-        $stmtcnt = $pdo->query("SELECT COUNT(*) FROM lab ");
+        $stmtcnt = $pdo->query("SELECT COUNT(*) FROM lab");
         $row = $stmtcnt->fetch(PDO::FETCH_ASSOC);
-
         if($row['COUNT(*)']!=='0')
         {
             $i=1;
-            $stmtread = $pdo->query("SELECT name FROM lab ORDER BY name");
+            $stmtread = $pdo->query("SELECT * FROM lab order by name");
             echo ("<table class=\"table table-striped\">
                 <tr> <th>S.no.</th><th>Lab Name</th> </tr>");
             while ( $row = $stmtread->fetch(PDO::FETCH_ASSOC) )
@@ -54,7 +52,10 @@
                 echo($i);
                 echo("</td>");
                 echo ("<td>");
-                echo(htmlentities($row['name']));
+                //Ghanta Consistent
+                echo ("<a class='link-black' href='viewpcbylab.php?lab=".$row['lab_id'])."'>";
+                echo (htmlentities($row['name']));
+                echo ("</a>");
                 echo ("</td>");
                 $i++;
             }
@@ -67,5 +68,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
+    <script>
+        $(document).readt(function(){
+            $('.labs').click()
+            {
+                <?php
+                ?>
+            }
+        });
+    </script>
 </body>
 </html>
