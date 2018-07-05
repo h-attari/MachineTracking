fd25cb4c4d31c9340fbe31e806c15da80729b24d
Anurag Phadnis <Phadnis.anurag@gmail.com>
2018-07-05 12:42:52 +0530

    dropdown

--- script.js
+++ script.js
@@ -6,10 +6,25 @@
              });
 function Device()
 {
-	val = document.getElementById('device-drop').value;
+	val = document.getElementById('drop-other').value;
+	if(val=="Other")
+	{
+		document.getElementById('hide-drop-other').disabled=false;
+		document.getElementById('alert-server-new').value="1";
+	}
+	else
+	{
+		document.getElementById('hide-drop-other').disabled=true;
+		
+	}
+}
+function Name()
+{
+	val = document.getElementById('drop-name').value;
 	if(val=="Other")
 	{
 		document.getElementById('other-device').disabled=false;
+		document.getElementById('alert-server-new-device').value="1";
 	}
 	else
 	{
