f0d255997ba6ceaa006a1c0b0a52c89727b47092
Krish Lalwani <35841954+KrishLalwani@users.noreply.github.com>
2018-07-05 09:24:58 +0530

    Other drop down script

--- script.js
+++ script.js
@@ -4,3 +4,16 @@
                      $(this).toggleClass('active');
                  });
              });
+function Device()
+{
+	val = document.getElementById('device-drop').value;
+	if(val=="Other")
+	{
+		document.getElementById('other-device').disabled=false;
+	}
+	else
+	{
+		document.getElementById('other-device').disabled=true;
+		
+	}
+}
